5665e821ffbebb23f6e1a0ee0833b65bc2b5a3d1
Rubin James <Rubin.James@smu.ca>
2021-12-14 15:31:27 -0400

    Commiting changes for user details and preview screen

---
+++ GameSureWebApp/Models/ViewModels/UserForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GameSureWebApp.Areas.Identity.Data;
+using GameSureWebApp.Models;
+
+namespace GameSureWebApp.Models.ViewModels
+{
+    public class UserForm
+    {
+       
+        public String FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailId { get; set; }
+        public int Phone { get; set; }
+        public string Addr1 { get; set; }
+        public string Addr2 { get; set; }
+        public string City { get; set; }
+        public Province Province { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+    }
+}
